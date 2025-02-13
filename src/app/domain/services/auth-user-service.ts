01f2542ff30683f66b59f7b3fcc8f67aad250bf8
Adrian L. Medeiros <lima.adrian13@gmail.com>
2025-02-13 11:08:26 -0300

    refactor: return user data inside the token payload

--- src/app/domain/services/auth-user-service.ts
+++ src/app/domain/services/auth-user-service.ts
@@ -7,7 +7,6 @@ import { StatusCodes } from "http-status-codes";
 import { APIError } from '../../api/helpers/APIError';
 
 interface AuthHttpResponse {
-    user: User
     token: string
 }
 
@@ -30,18 +29,12 @@ export class AuthUserService {
         if (!passwordMatched) {
             throw new APIError('Invalid password', StatusCodes.UNAUTHORIZED)
         }
-
+            
+        const { id } = user
         const { secret, expiresIn } = authConfig.jwt
+        
+        const token = jwt.sign({ id, email } as User, secret!, { expiresIn })
 
-        const token = jwt.sign({ id: user.id }, secret!, { expiresIn })
-
-        return {
-            user: {
-                id: user.id,
-                email
-            } as User,
-            token
-        }
-
+        return token
     }
 }
